2c27c378f4589eb4398005b2b974f265d4cf5acf
jingdai2014 <jd837@cornell.edu>
2016-04-19 19:21:09 -0400

    push 0419 data

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://moomap:moomapduff@moomap.cm30mcffmq7o.us-west-2.rds.amazonaws.com:3306/moomap'
 
 # Uncomment the line below if you want to work with a local DB
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
 
 SQLALCHEMY_POOL_RECYCLE = 3600
 
